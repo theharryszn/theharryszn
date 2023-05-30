f7fdf4830c56243ba4f947163321c73b8b4416e7
Abidemi Ope <opeabidemi@gmail.com>
2023-05-30 22:34:00 +0100

    updates

--- src/components/PageContainer.vue
+++ src/components/PageContainer.vue
@@ -1,4 +1,88 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { ScrollTrigger } from 'gsap/dist/ScrollTrigger'
+import { gsap } from 'gsap'
+import imagesLoaded from 'imagesloaded'
+import { onMounted } from 'vue'
+
+onMounted(() => {
+  const shrinks = document.querySelectorAll('.cursor-shrink')
+  const dark_cursors = document.querySelectorAll('.dark-cursor')
+
+  shrinks.forEach((el) => {
+    el.addEventListener('mouseover', () => {
+      gsap.to('.cursor', {
+        scale: 0.5,
+        duration: 0.5
+      })
+    })
+
+    el.addEventListener('mouseout', () => {
+      gsap.to('.cursor', {
+        scale: 1,
+        duration: 0.5
+      })
+    })
+  })
+
+  dark_cursors.forEach((el) => {
+    el.addEventListener('mouseover', () => {
+      gsap.to('.cursor', {
+        borderColor: 'rgb(9 9 11 / 1)',
+        duration: 0.5
+      })
+      gsap.to('.cursor .dot', {
+        backgroundColor: 'rgb(9 9 11 / 1)',
+        duration: 0.5
+      })
+    })
+
+    el.addEventListener('mouseout', () => {
+      gsap.to('.cursor', {
+        borderColor: 'rgb(250 250 250 / 0.5)',
+        duration: 0.5
+      })
+
+      gsap.to('.cursor .dot', {
+        backgroundColor: 'rgb(250 250 250 / 0.5)',
+        duration: 0.5
+      })
+    })
+  })
+
+  const images = document.querySelectorAll('img')
+
+  const imgLoad = imagesLoaded(images)
+
+  const entranceAnimation = () => {
+    const tl = gsap.timeline()
+    tl.to(
+      '.preloader',
+      {
+        yPercent: -100,
+        duration: 1.25,
+        ease: 'power4.inOut',
+        delay: 1
+      },
+      0
+    )
+  }
+
+  imgLoad.on('always', function () {
+    entranceAnimation()
+  })
+
+  let loaded = 0
+
+  const pI = document.getElementById('progress-indicator')
+
+  imgLoad.on('progress', () => {
+    loaded++
+    if (pI) {
+      pI.style.width = ((loaded / images.length) * 100).toFixed(0) + '%'
+    }
+  })
+})
+</script>
 <template>
   <div class="page full-page">
     <div class="container" data-scroll-container ref="container">
@@ -7,9 +91,6 @@
   </div>
 </template>
 <script lang="ts">
-import { ScrollTrigger } from 'gsap/dist/ScrollTrigger'
-import { gsap } from 'gsap'
-
 export default {
   name: 'pageContainer',
   methods: {
@@ -45,49 +126,6 @@ export default {
   },
   mounted() {
     this.setScroll()
-    const shrinks = document.querySelectorAll('.cursor-shrink')
-    const dark_cursors = document.querySelectorAll('.dark-cursor')
-
-    shrinks.forEach((el) => {
-      el.addEventListener('mouseover', () => {
-        gsap.to('.cursor', {
-          scale: 0.5,
-          duration: 0.5
-        })
-      })
-
-      el.addEventListener('mouseout', () => {
-        gsap.to('.cursor', {
-          scale: 1,
-          duration: 0.5
-        })
-      })
-    })
-
-    dark_cursors.forEach((el) => {
-      el.addEventListener('mouseover', () => {
-        gsap.to('.cursor', {
-          borderColor: 'rgb(9 9 11 / 1)',
-          duration: 0.5
-        })
-        gsap.to('.cursor .dot', {
-          backgroundColor: 'rgb(9 9 11 / 1)',
-          duration: 0.5
-        })
-      })
-
-      el.addEventListener('mouseout', () => {
-        gsap.to('.cursor', {
-          borderColor: 'rgb(250 250 250 / 0.5)',
-          duration: 0.5
-        })
-
-        gsap.to('.cursor .dot', {
-          backgroundColor: 'rgb(250 250 250 / 0.5)',
-          duration: 0.5
-        })
-      })
-    })
   }
 }
 </script>
--- src/components/PreloaderView.vue
+++ src/components/PreloaderView.vue
@@ -1,46 +1,9 @@
-<script setup lang="ts">
-import { onMounted, ref } from 'vue'
-import imagesLoaded from 'imagesloaded'
-import { gsap } from 'gsap'
-
-let progress = ref(0)
-
-onMounted(() => {
-  const images = document.querySelectorAll('img')
-
-  const imgLoad = imagesLoaded(images)
-
-  const entranceAnimation = () => {
-    const tl = gsap.timeline()
-    tl.to(
-      '.preloader',
-      {
-        yPercent: -100,
-        duration: 1.25,
-        ease: 'power4.inOut',
-        delay: 1
-      },
-      0
-    )
-  }
-
-  imgLoad.on('always', function () {
-    entranceAnimation()
-  })
-
-  let loaded = 0
-
-  imgLoad.on('progress', function (_, image) {
-    var result = image?.isLoaded ? 'loaded' : 'broken'
-    console.log('image is ' + result + ' for ' + image?.img.src)
-    loaded++
-    progress.value = (loaded / images.length) * 100
-  })
-})
-</script>
+<script setup lang="ts"></script>
 <template>
   <div class="preloader p-10">
-    <div class="animate-pulse">Loading {{ progress }}%</div>
+    <div class="w-40 lg:w-56 h-1 bg-zinc-700 rounded-full">
+      <div id="progress-indicator" class="h-1 w-[0%] bg-zinc-50 rounded-full"></div>
+    </div>
     <!-- <div
       class="bg-zinc-900/20 m-auto border border-zinc-50/5 p-5 font-mono text-sm rounded-md max-w-[30rem] min-h-[10rem] w-full flex flex-col gap-5"
     >
